469153359709bd7a5504cf56674aaea163fb42bc
Corwin Rainier <corwinrainier89@gmail.com>
2022-06-30 09:26:19 -0300

    Initial Commit

---
+++ Goal Tracker/Goal_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Goal_TrackerApp.swift
+//  Goal Tracker
+//
+//  Created by Corwin Rainier on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Goal_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
